59cd884e42d691118d4716b3ce483712304af009
JordenR <jordenragels3@gmail.com>
2021-01-30 23:04:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var box = document.getElementById("box");
+
+function grow() {
+  box.style.width = "200px";
+  box.style.height = "200px";
+}
+
+function shrink() {
+  box.style.width = "50px";
+  box.style.height = "50px";
+}
